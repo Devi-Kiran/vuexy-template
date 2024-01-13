1ec0719aa2d7198717246ace5468153071dab223
Devi-Kiran <devikiran333007@gmail.com>
2024-01-13 14:38:40 +0530

    added Offline Component

---
+++ src/@core/components/ametecs/OfflineConponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OfflineConponent() {
+  return (
+    <div style={{height: "100vh"}} className="d-flex justify-content-center align-items-center">
+        please chech your network connection!
+    </div>
+  )
+}
+
+export default OfflineConponent
--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 // ** Router Import
-import Router from './router/Router';
+// import Router from './router/Router';
 
-const App = props => <Router />
+// const App = props => <Router />
+
+// export default App
+
+import Router from './router/Router';
+import OfflineConponent from './@core/components/ametecs/OfflineConponent';
+
+const App = props => {
+    const isOnline = navigator.onLine;
+    // console.log(isOnline)
+    return (
+        <>
+            {isOnline ? <Router/> : <OfflineConponent/>}
+        </>
+    )
+}
 
 export default App
+
